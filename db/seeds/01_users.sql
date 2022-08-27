6fd0245cd5123fb024bcd78da9596ce9895ca74d
Hind <hind.nayhi@gmail.com>
2022-08-26 18:47:52 -0600

    Update seeds/01_users.sql

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,8 +1,8 @@
 INSERT INTO users (username, email, password, city, province)
-VALUES ('Alice', 'alice@gmail.com', 'password', 'Calgary', 'Alberta');
+VALUES ('Alice', 'alice@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', 'Calgary', 'Alberta');
 
 INSERT INTO users (username, email, password, city, province)
-VALUES ('Kira', 'kira@gmail.com', 'password', 'Toronto', 'Ontario');
+VALUES ('Kira', 'kira@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', 'Toronto', 'Ontario');
 
 INSERT INTO users (username, email, password, city, province)
-VALUES ('Jack', 'jack@gmail.com', 'password', 'Vancouver', 'British Columbia');
+VALUES ('Jack', 'jack@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', 'Vancouver', 'British Columbia');
