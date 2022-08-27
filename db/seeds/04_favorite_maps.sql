61ed869cd9456784493e3d673c13fe6ca8f15972
Hind <hind.nayhi@gmail.com>
2022-08-27 09:11:48 -0600

    Add more details.

--- db/seeds/04_favorite_maps.sql
+++ db/seeds/04_favorite_maps.sql
@@ -1,8 +1,8 @@
-INSERT INTO favorite_maps (title)
-VALUES ('Best Places to Eat Around Town');
+INSERT INTO favorite_maps (title, created_at, user_id, map_id)
+VALUES ('Best Places to Eat Around Town', '2022-08-26', 3, 1);
 
-INSERT INTO favorite_maps (title)
-VALUES ('Best Mountains to Hike');
+INSERT INTO favorite_maps (title, created_at, user_id, map_id)
+VALUES ('Best Mountains to Hike', '2022-08-26', 1, 2);
 
-INSERT INTO favorite_maps (title)
-VALUES ('Best Parks to visit');
+INSERT INTO favorite_maps (title, created_at, user_id, map_id)
+VALUES ('Best Parks to visit', '2022-08-26', 2, 3);
