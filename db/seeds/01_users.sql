4625730b00b16324e299d468f3f3f0b1aa66f4a6
Hind <hind.nayhi@gmail.com>
2022-08-26 18:39:04 -0600

    Create 2 table: users and favorite_maps.

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,3 +1,8 @@
--- Users table seeds here (Example)
-INSERT INTO users (name) VALUES ('Alice');
-INSERT INTO users (name) VALUES ('Kira');
+INSERT INTO users (username, email, password, city, province)
+VALUES ('Alice', 'alice@gmail.com', 'password', 'Calgary', 'Alberta');
+
+INSERT INTO users (username, email, password, city, province)
+VALUES ('Kira', 'kira@gmail.com', 'password', 'Toronto', 'Ontario');
+
+INSERT INTO users (username, email, password, city, province)
+VALUES ('Jack', 'jack@gmail.com', 'password', 'Vancouver', 'British Columbia');
---
+++ db/seeds/04_favorite_maps.sql
@@ -0,0 +1,8 @@
+INSERT INTO favorite_maps (title)
+VALUES ('Best Places to Eat Around Town');
+
+INSERT INTO favorite_maps (title)
+VALUES ('Best Mountains to Hike');
+
+INSERT INTO favorite_maps (title)
+VALUES ('Best Parks to visit');
