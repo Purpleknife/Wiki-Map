6f4d01eef4adcdddee33362e9ec47d59cf54c937
Bayan Safaee <bayan.safaee@gmail.com>
2022-08-27 09:54:45 -0700

    sql

---
+++ db/seeds/02_maps.sql
@@ -0,0 +1,7 @@
+INSERT INTO favorite_maps (user_id, title latitude, longitude, city, province)
+
+VALUES (3, 'Best Places to Eat Around Town', 49.2827, 123.1207,'Vancouver', 'British Columbia');
+
+VALUES (2, 'Best Mountains to Hike', 49.2827, 123.1207,'Vancouver', 'British Columbia');
+
+VALUES (1, 'Best Parks to visit', 49.2827, 123.1207,'Vancouver', 'British Columbia');
